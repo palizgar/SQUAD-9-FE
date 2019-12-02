a691e1ab88287485013f0a5101b1bf4e87eea04d
Hatef Palizgar <hatef.palizgar@gmail.com>
2019-12-02 22:05:11 +0200

    SQUAD-9 add update button

--- src/app/allclients/allclients.component.ts
+++ src/app/allclients/allclients.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import Client from 'src/app/client';
 
 @Component({
@@ -9,7 +9,7 @@ import Client from 'src/app/client';
 })
 export class AllclientsComponent implements OnInit {
   clientData: Client[];
-
+  
   constructor(private http: HttpClient) { 
   }
 
@@ -20,6 +20,12 @@ export class AllclientsComponent implements OnInit {
     return await this.http.get('api/v1/clients').subscribe((data: Client[]) => this.clientData = [ ...data]);
   }
   async deleteById(clientId) {
-    return await this.http.delete('api/v1/clients/' + clientId).subscribe(() => this.getList());
+    return await this.http.delete('api/v1/clients/' + clientId,).subscribe(() => this.getList());
+  }
+
+  async updateById(clientId, firstname) {
+    const params = new HttpParams()
+    .set('firstName', firstname);
+    return await this.http.put('api/v1/clients/'+ clientId,{}, {params}).subscribe(() => this.getList());
   }
 }
